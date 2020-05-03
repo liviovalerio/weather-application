ced35f983498979a538c4b3523489e2db3338b5e
Livio Valerio <Livio-Valerio@outlook.fr>
2020-05-03 20:25:56 +0200

    ask the user for his choice

--- script.js
+++ script.js
@@ -1,21 +1,33 @@
 let town = 'Liège';
-const url = `https://api.openweathermap.org/data/2.5/weather?q=${town}&appid=313db54332243f97fd92c0f47ea45453&units=metric`
+let button = document.querySelector('#changer');
+let url = `https://api.openweathermap.org/data/2.5/weather?q=${town}&appid=313db54332243f97fd92c0f47ea45453&units=metric`
 
-let requete = new XMLHttpRequest();
-requete.open('GET', url);
-requete.responseType = 'json';
-requete.send();
+function getTemperature(town) {
+    let requete = new XMLHttpRequest();
+    requete.open('GET', url);
+    requete.responseType = 'json';
+    requete.send();
 
-requete.onload = () => {
-    if (requete.readyState === XMLHttpRequest.DONE) {
-        if(requete.status === 200) {
-            let response = requete.response;
-            let temperature = response.main.temp;
-            document.querySelector('#temperature_label').textContent = temperature
-        } else {
-            alert("Il y a un problème avec l'API!")
+    requete.onload = () => {
+        if (requete.readyState === XMLHttpRequest.DONE) {
+            if(requete.status === 200) {
+                let response = requete.response;
+                console.log(response);
+                let temperature = response.main.temp;
+                document.querySelector('#temperature_label').textContent = temperature
+            } else {
+                alert("Il y a un problème avec l'API!")
+            }
         }
     }
 }
 
+getTemperature(town);
 document.querySelector('#ville').textContent = town;
+
+button.addEventListener('click', () => {
+    town = prompt('Pouvez-vous choisir une ville?');
+    url = `https://api.openweathermap.org/data/2.5/weather?q=${town}&appid=313db54332243f97fd92c0f47ea45453&units=metric`;
+    getTemperature(town)
+    document.querySelector('#ville').textContent = town;
+})
